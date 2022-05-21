e3a7cf8c7b138a4e4f94ed537e83f90aa8393f2c
Immanuelolivia1--git <emmanueljuliet2019@gmail.com>
2022-05-21 15:10:32 +0100

    feat: getting started

---
+++ script.js
@@ -0,0 +1,7 @@
+// const user = prompt("Enter your name");
+// const welcomeMessage = prompt("Welcome " + user + " what tier are you willing to opt for?");
+// const successMessage = alert("Congratulations " + user + " you've been successfully registered.");
+
+const userName = document.getElementById('name');
+const amount = document.getElementById('amount');
+const tier = document.getElementById('tier');
